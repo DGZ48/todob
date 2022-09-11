6f1acea22e31ac6029ee98e0081c0bf8cca234c5
win2cot <ishikawa.masayuki.0605@gmail.com>
2022-09-12 00:48:56 +0900

    springboot initializer

---
+++ src/main/java/xyz/dgz48/todob/TodobApplication.java
@@ -0,0 +1,13 @@
+package xyz.dgz48.todob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/dgz48/todob/TodobApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.dgz48.todob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
